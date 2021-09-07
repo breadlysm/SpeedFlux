6c9e81f066bd62b658fbeaebc261a592815f3fcd
breadlysm <jbradly.smith@gmail.com>
2021-09-06 23:44:06 -0400

    added new class for influx 2

--- speedflux/influx.py
+++ speedflux/influx.py
@@ -2,11 +2,48 @@ import sys
 from urllib3.exceptions import NewConnectionError
 
 from influxdb import InfluxDBClient
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+
 import speedflux
 from requests.exceptions import ConnectionError
 
 
-class Influx:
+username = 'username'
+password = 'password'
+
+database = 'telegraf'
+retention_policy = 'autogen'
+
+bucket = f'{database}/{retention_policy}'
+
+with InfluxDBClient(url='http://localhost:8086', token=f'{username}:{password}', org='-') as client:
+ 
+class Influx2:
+    def __init__(self, config):
+        self.config = config
+        self._client = None
+        self.retries = 0
+
+    
+    @property
+    def client(self):
+        if not self._client:
+            self._client = InfluxDBClient(
+                url=self.config.INFLUX_DB_2_ADDRESS,
+                token=self.config.INFLUX_DB_2_TOKEN,
+                org=self.config.INFLUX_DB_2_ORG,
+                debug=False
+                )
+            speedflux.LOG.debug("Client extablished")
+        return self._client
+
+    def write(self, data, data_type='Speedtest'):
+        write_api = client.write_api(write_options=SYNCHRONOUS)
+        write_api.write(bucket=self.config.INFlUX_DB_2_BUCKET, record=data)
+
+
+    class Influx:
     def __init__(self, config):
         self.config = config
         self._client = None
@@ -54,74 +91,6 @@ class Influx:
             speedflux.LOG.error(f"Retry {self.retries}: Initiliazing DB.")
             self.init_db()
 
-    # def format_data(self, data):
-    #     influx_data = [
-    #         {
-    #             'measurement': 'ping',
-    #             'time': data['timestamp'],
-    #             'fields': {
-    #                 'jitter': data['ping'].get('jitter', 0),
-    #                 'latency': data['ping'].get('latency', 0)
-    #             }
-    #         },
-    #         {
-    #             'measurement': 'download',
-    #             'time': data['timestamp'],
-    #             'fields': {
-    #                 # Byte to Megabit
-    #                 'bandwidth': data['download'].get('bandwidth', 0) / 125000,
-    #                 'bytes': data['download'].get('bytes', 0),
-    #                 'elapsed': data['download']['elapsed']
-    #             }
-    #         },
-    #         {
-    #             'measurement': 'upload',
-    #             'time': data['timestamp'],
-    #             'fields': {
-    #                 # Byte to Megabit
-    #                 'bandwidth': data['upload'].get('bandwidth', 0) / 125000,
-    #                 'bytes': data['upload']['bytes'],
-    #                 'elapsed': data['upload']['elapsed']
-    #             }
-    #         },
-    #         {
-    #             'measurement': 'packetLoss',
-    #             'time': data['timestamp'],
-    #             'fields': {
-    #                 'packetLoss': int(data.get('packetLoss', 0))
-    #             }
-    #         },
-    #         {
-    #             'measurement': 'speeds',
-    #             'time': data['timestamp'],
-    #             'fields': {
-    #                 'jitter': data['ping'].get('jitter', 0),
-    #                 'latency': data['ping'].get('latency', 0),
-    #                 'packetLoss': int(data.get('packetLoss', 0)),
-    #                 # Byte to Megabit
-    #                 'bandwidth_down': data['download'].get(
-    #                     'bandwidth', 0) / 125000,
-    #                 'bytes_down': data['download'].get(
-    #                     'bytes', 0),
-    #                 'elapsed_down': data['download'].get(
-    #                     'elapsed'),
-    #                 # Byte to Megabit
-    #                 'bandwidth_up': data['upload'].get(
-    #                     'bandwidth', 0) / 125000,
-    #                 'bytes_up': data['upload'].get(
-    #                     'bytes', 0),
-    #                 'elapsed_up': data['upload'].get(
-    #                     'elapsed')
-    #             }
-    #         }
-    #     ]
-    #     tags = self.tag_selection(data)
-    #     if tags is not None:
-    #         for measurement in influx_data:
-    #             measurement['tags'] = tags
-
-    #     return influx_data
-
     def write(self, data, data_type='Speedtest'):
         try:
             if self.client.write_points(data):
@@ -148,46 +117,6 @@ class Influx:
 
         except Exception as err:
             speedflux.LOG.error(F"{err}")
-
-    # def tag_selection(self, data):
-    #     tags = self.config.INFLUX_DB_TAGS
-    #     options = {}
-
-    #     # tag_switch takes in _data and attaches CLIoutput to more readable ids
-    #     tag_switch = {
-    #         'namespace': self.config.NAMESPACE,
-    #         'isp': data['isp'],
-    #         'interface': data['interface']['name'],
-    #         'internal_ip': data['interface']['internalIp'],
-    #         'interface_mac': data['interface']['macAddr'],
-    #         'vpn_enabled': (
-    #             False if data['interface']['isVpn'] == 'false' else True),
-    #         'external_ip': data['interface']['externalIp'],
-    #         'server_id': data['server']['id'],
-    #         'server_name': data['server']['name'],
-    #         'server_location': data['server']['location'],
-    #         'server_country': data['server']['country'],
-    #         'server_host': data['server']['host'],
-    #         'server_port': data['server']['port'],
-    #         'server_ip': data['server']['ip'],
-    #         'speedtest_id': data['result']['id'],
-    #         'speedtest_url': data['result']['url']
-    #     }
-
-    #     if tags is None:
-    #         tags = 'namespace'
-    #     elif '*' in tags:
-    #         return tag_switch
-    #     else:
-    #         tags = 'namespace, ' + tags
-
-    #     tags = tags.split(',')
-    #     for tag in tags:
-    #         # split the tag string, strip and add selected tags to {options}
-    #         # with corresponding tag_switch data
-    #         tag = tag.strip()
-    #         options[tag] = tag_switch[tag]
-    #     return options
 
     def process_data(self, data):
         data = self.format_data(data)
